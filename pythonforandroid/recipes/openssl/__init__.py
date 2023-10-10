20e71bff3e8edbc88fb083eecc3c27f587bc2212
elise1983 <mrsstirling83@gmail.com>
2023-10-10 03:37:36 +0100

    Uncommenting one recipe at a time due to linked dependencies.

--- pythonforandroid/recipes/openssl/__init__.py
+++ pythonforandroid/recipes/openssl/__init__.py
@@ -7,55 +7,18 @@ import sh
 
 
 class OpenSSLRecipe(Recipe):
-    '''
-    The OpenSSL libraries for python-for-android. This recipe will generate the
-    following libraries as shared libraries (*.so):
 
-        - crypto
-        - ssl
-
-    The generated openssl libraries are versioned, where the version is the
-    recipe attribute :attr:`version` e.g.: ``libcrypto1.1.so``,
-    ``libssl1.1.so``...so...to link your recipe with the openssl libs,
-    remember to add the version at the end, e.g.:
-    ``-lcrypto1.1 -lssl1.1``. Or better, you could do it dynamically
-    using the methods: :meth:`include_flags`, :meth:`link_dirs_flags` and
-    :meth:`link_libs_flags`.
-
-    .. warning:: This recipe is very sensitive because is used for our core
-        recipes, the python recipes. The used API should match with the one
-        used in our python build, otherwise we will be unable to build the
-        _ssl.so python module.
-
-    .. versionchanged:: 0.6.0
-
-        - The gcc compiler has been deprecated in favour of clang and libraries
-          updated to version 1.1.1 (LTS - supported until 11th September 2023)
-        - Added two new methods to make easier to link with openssl:
-          :meth:`include_flags` and :meth:`link_flags`
-        - subclassed versioned_url
-        - Adapted method :meth:`select_build_arch` to API 21+
-        - Add ability to build a legacy version of the openssl libs when using
-          python2legacy or python3crystax.
-
-    .. versionchanged:: 2019.06.06.1.dev0
-
-        - Removed legacy version of openssl libraries
-
-    '''
-
-    version = '3.1'
-    '''the major minor version used to link our recipes'''
+    version = '3'
 
     url_version = '3.1.3'
     '''the version used to download our libraries'''
 
-    url = 'https://github.com/openssl/openssl/archive/refs/tags/openssl-3.1.3.tar.gz'
+    url = 'https://github.com/openssl/openssl/archive/refs/tags/{url_version}.1.3.tar.gz'
 
-  #  built_libraries = {
-   #     'libcrypto{version}.so'.format(version=version): '.',
-   #     'libssl{version}.so'.format(version=version): '.',
-  #  }
+    built_libraries =
+        'libcrypto{version}.so'.format(version=version): '.',
+        'libssl{version}.so'.format(version=version): '.',
+    }
 
     #@property
     #def versioned_url(self):
@@ -63,10 +26,10 @@ class OpenSSLRecipe(Recipe):
     #        return None
     #    return self.url.format(url_version=self.url_version)
 
-  #  def get_build_dir(self, arch):
-   #     return join(
-   #         self.get_build_container_dir(arch), self.name + self.version
- #       )
+    def get_build_dir(self, arch):
+        return join(
+            self.get_build_container_dir(arch), self.name + self.version
+        )
 
     def include_flags(self, arch):
         '''Returns a string with the include folders'''
@@ -92,46 +55,46 @@ class OpenSSLRecipe(Recipe):
         in the format: `-L<lib directory> -l<lib>`'''
         return self.link_dirs_flags(arch) + self.link_libs_flags()
 
-  #  def get_recipe_env(self, arch=None):
-  #      env = super().get_recipe_env(arch)
-   #     env['OPENSSL_VERSION'] = self.version
-  #      env['MAKE'] = 'make'  # This removes the '-j5', which isn't safe
-  #      env['CC'] = 'clang'
-   #     env['ANDROID_NDK_HOME'] = self.ctx.ndk_dir
-   #     return env
+    def get_recipe_env(self, arch=None):
+        env = super().get_recipe_env(arch)
+        env['OPENSSL_VERSION'] = self.version
+        env['MAKE'] = 'make'  # This removes the '-j5', which isn't safe
+        env['CC'] = 'clang'
+        env['ANDROID_NDK_HOME'] = self.ctx.ndk_dir
+        return env
 
-  #  def select_build_arch(self, arch):
-  #      aname = arch.arch
-  #      if 'arm64' in aname:
- #           return 'android-arm64'
- #       if 'v7a' in aname:
- #           return 'android-arm'
- #       if 'arm' in aname:
-  #          return 'android'
-  #      if 'x86_64' in aname:
-   #         return 'android-x86_64'
-  #      if 'x86' in aname:
-  #          return 'android-x86'
- #       return 'linux-armv4'
+    def select_build_arch(self, arch):
+        aname = arch.arch
+        if 'arm64' in aname:
+            return 'android-arm64'
+        if 'v7a' in aname:
+            return 'android-arm'
+        if 'arm' in aname:
+            return 'android'
+        if 'x86_64' in aname:
+            return 'android-x86_64'
+        if 'x86' in aname:
+            return 'android-x86'
+        return 'linux-armv4'
 
- #   def build_arch(self, arch):
- #       env = self.get_recipe_env(arch)
- #       with current_directory(self.get_build_dir(arch.arch)):
+    def build_arch(self, arch):
+        env = self.get_recipe_env(arch)
+        with current_directory(self.get_build_dir(arch.arch)):
             # sh fails with code 255 trying to execute ./Configure
             # so instead we manually run perl passing in Configure
-    #        perl = sh.Command('perl')
-    #        buildarch = self.select_build_arch(arch)
-     #       config_args = [
-    #            'shared',
-    #            'no-dso',
-    #            'no-asm',
-    #            buildarch,
-     #           '-D__ANDROID_API__={}'.format(self.ctx.ndk_api),
-     #       ]
-   #         shprint(perl, 'Configure', *config_args, _env=env)
-    #        self.apply_patch('disable-sover.patch', arch.arch)
+            perl = sh.Command('perl')
+            buildarch = self.select_build_arch(arch)
+            config_args = [
+                'shared',
+                'no-dso',
+                'no-asm',
+                buildarch,
+                '-D__ANDROID_API__={}'.format(self.ctx.ndk_api),
+            ]
+            shprint(perl, 'Configure', *config_args, _env=env)
+            self.apply_patch('disable-sover.patch', arch.arch)
 
-     #       shprint(sh.make, 'build_libs', _env=env)
+            shprint(sh.make, 'build_libs', _env=env)
 
 
 recipe = OpenSSLRecipe()
